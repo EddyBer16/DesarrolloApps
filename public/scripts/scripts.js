e2948c892f7105b0c262171087dfe21877fa4165
EddyBer16 <angel.tigre@hotmail.com>
2019-02-13 16:44:26 -0600

    another commit

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -7,15 +7,6 @@ $(document).ready(() => {
             $(".icono_menu").toggleClass("cruz");
         });
     });
-
-    $("header,div,footer").hide();
-    $("div:nth-child(1)").show();
-    $("#hidden").delay(4500).slideUp(1500);
-    $("#hidden span").delay(5000).slideUp(2000);
-
-    setTimeout(function(){
-        $("header,div,footer").show();
-    },4500);
 });
 
 $("document").ready(function(){
